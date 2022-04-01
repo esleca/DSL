53a8bab09a16655bf0a9dcdd3bfedd00144375f1
Esteban <elc_902@hotmail.com>
2022-03-31 22:03:31 -0600

    constants as static class reference on assert type pair class

--- src/main/java/models/entities/unittests/asserts/types/AssertTypePair.java
+++ src/main/java/models/entities/unittests/asserts/types/AssertTypePair.java
@@ -1,17 +1,17 @@
 package models.entities.unittests.asserts.types;
 
 import models.entities.unittests.FunctionArgument;
-import utils.Constants;
-
 import java.util.ArrayList;
+
+import static utils.Constants.*;
 
 public abstract class AssertTypePair extends AssertType{
 
     @Override
     public ArrayList<FunctionArgument> getAssertArguments() {
         ArrayList<FunctionArgument> assertParameters = new ArrayList<>();
-        assertParameters.add(new FunctionArgument(Constants.ARGUMENT_EXPECTED));
-        assertParameters.add(new FunctionArgument(Constants.ARGUMENT_RESULT));
+        assertParameters.add(new FunctionArgument(ARGUMENT_EXPECTED));
+        assertParameters.add(new FunctionArgument(ARGUMENT_RESULT));
         return assertParameters;
     }
 
