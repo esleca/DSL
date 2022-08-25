c99a9399c33b5eb34c4e3ebab872c9d9e404af38
Esteban <elc_902@hotmail.com>
2022-08-25 17:58:33 -0600

    add additional unit tests and validations on UnitTestRequest object

--- src/main/java/com/dsl/validators/DSLFluentValidator.java
+++ src/main/java/com/dsl/validators/DSLFluentValidator.java
@@ -16,69 +16,57 @@ public class DSLFluentValidator extends AbstractValidator<UnitTestRequest> {
 		
 		// Class Path
 		ruleFor(UnitTestRequest::getClassPath)
-		.must(not(nullValue()))
-    		.withMessage("classPath must not be null")
-    		.withFieldName("classPath");
-		
-		ruleFor(UnitTestRequest::getClassPath)
-		.must(not(stringEmptyOrNull()))
-			.withMessage("classPath must not be empty")
-			.withFieldName("classPath");
-		
+			.must(not(nullValue()))
+	    		.withMessage("classPath must not be null")
+	    		.withFieldName("classPath")
+			.must(not(stringEmptyOrNull()))
+				.withMessage("classPath must not be empty")
+				.withFieldName("classPath");
+			
 		// Language
 		ruleFor(UnitTestRequest::getLanguage)
-		.must(not(nullValue()))
-    		.withMessage("language must not be null")
-    		.withFieldName("language");
-		
-		ruleFor(UnitTestRequest::getLanguage)
-		.must(not(stringEmptyOrNull()))
-			.withMessage("language must not be empty")
-			.withFieldName("language");
-		
+			.must(not(nullValue()))
+	    		.withMessage("language must not be null")
+	    		.withFieldName("language")
+			.must(not(stringEmptyOrNull()))
+				.withMessage("language must not be empty")
+				.withFieldName("language");
+			
 		// OutputPath
 		ruleFor(UnitTestRequest::getOutputPath)
-		.must(not(nullValue()))
-    		.withMessage("outputPath must not be null")
-    		.withFieldName("outputPath");
-		
-		ruleFor(UnitTestRequest::getOutputPath)
-		.must(not(stringEmptyOrNull()))
-			.withMessage("outputPath must not be empty")
-			.withFieldName("outputPath");
+			.must(not(nullValue()))
+	    		.withMessage("outputPath must not be null")
+	    		.withFieldName("outputPath")
+			.must(not(stringEmptyOrNull()))
+				.withMessage("outputPath must not be empty")
+				.withFieldName("outputPath");
 		
 		// Function
 		ruleFor(UnitTestRequest::getFunction)
-		.must(not(nullValue()))
-    		.withMessage("function must not be null")
-    		.withFieldName("function");
-		
-		ruleFor(UnitTestRequest::getFunction)
-		.must(not(stringEmptyOrNull()))
-			.withMessage("function must not be empty")
-			.withFieldName("function");
+			.must(not(nullValue()))
+	    		.withMessage("function must not be null")
+	    		.withFieldName("function")
+			.must(not(stringEmptyOrNull()))
+				.withMessage("function must not be empty")
+				.withFieldName("function");
 		
 		// TestName
 		ruleFor(UnitTestRequest::getTestName)
-		.must(not(nullValue()))
-        	.withMessage("testName must not be null")
-    		.withFieldName("testName");
-		
-		ruleFor(UnitTestRequest::getTestName)
-		.must(not(stringEmptyOrNull()))
-			.withMessage("testName must not be empty")
-			.withFieldName("testName");
-		
+			.must(not(nullValue()))
+	        	.withMessage("testName must not be null")
+	    		.withFieldName("testName")
+			.must(not(stringEmptyOrNull()))
+				.withMessage("testName must not be empty")
+				.withFieldName("testName");
+			
 		// Assertion
 		ruleFor(UnitTestRequest::getAssert)
-		.must(not(nullValue()))
-        	.withMessage("assertion must not be null")
-    		.withFieldName("assertion");
-		
-		ruleFor(UnitTestRequest::getAssert)
-		.must(not(stringEmptyOrNull()))
-			.withMessage("assertion must not be empty")
-			.withFieldName("assertion");
+			.must(not(nullValue()))
+	        	.withMessage("assertion must not be null")
+	    		.withFieldName("assertion")
+			.must(not(stringEmptyOrNull()))
+				.withMessage("assertion must not be empty")
+				.withFieldName("assertion");
 		
 	}
 }
