1076ece64bd0ccea4773b1e348e8e79de3fad0e5
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2022-11-04 19:23:25 -0600

    Cambios en el objeto UnitTestResponse

--- src/main/java/com/dsl/mappers/UnitTestMapper.java
+++ src/main/java/com/dsl/mappers/UnitTestMapper.java
@@ -1,11 +1,15 @@
 package com.dsl.mappers;
 
+import com.dsl.models.parameters.ParameterFunction;
+import com.dsl.models.parameters.ParameterScenario;
 import org.json.simple.JSONArray;
 import com.dsl.models.unittests.UnitTest;
 import com.dsl.models.dtos.UnitTestResponse;
 import com.dsl.models.dtos.UnitTestRequest;
 import com.dsl.models.database.UnitTestMetaData;
 import com.dsl.models.valuetypes.ValueType;
+
+import java.util.ArrayList;
 
 public class UnitTestMapper {
 	
@@ -29,7 +33,14 @@ public class UnitTestMapper {
 		String functionName = unitTest.getTestScenario().getFunction().getName();
 		String testName = unitTest.getTestScenario().getTestName();
 		String assertion = unitTest.getTestScenario().getAssertion();
-
-		return new UnitTestResponse(language, packageName, className, functionName, testName, assertion);
+		ArrayList<ParameterFunction> parameters = unitTest.getTestScenario().getFunction().getParameters();
+		ArrayList<ParameterScenario> paramScenario = unitTest.getTestScenario().getParameters();
+		ArrayList<Object> paramValues = new ArrayList<>();
+		for(int i = 0; i < paramScenario.size(); i++){
+			paramValues.add(paramScenario.get(i).getValueType().getValue().toString());
+		}
+		String expectedType = unitTest.getTestScenario().getExpectedResult().getValueType().getType();
+		Object expectedValue = unitTest.getTestScenario().getExpectedResult().getValueType().getValue();
+		return new UnitTestResponse(language, packageName, className, functionName, testName, assertion, parameters, expectedType, expectedValue, paramValues);
 	}
 }
