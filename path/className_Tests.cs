e6bef025011534a85184a09c4489a6937403c86c
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2023-04-04 23:21:07 -0600

    Small change for Java annotation

--- path/className_Tests.cs
+++ path/className_Tests.cs
@@ -1,4 +1,4 @@
-// Generation date: 2022-11-17T19:48:18.543645900.
+// Generation date: 2023-04-04T12:36:21.887634900.
 // Input language: null | Output language: C#.
 
 namespace package.name
--- path/className_Tests.java
+++ path/className_Tests.java
@@ -1,4 +1,4 @@
-// Generation date: 2022-11-17T19:48:18.551191900.
+// Generation date: 2023-04-04T12:36:21.898671400.
 // Input language: null | Output language: Java.
 
 package package.name;
