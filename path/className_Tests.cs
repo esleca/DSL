18ba6221e4de4e287c721970e5160c6e1d1d1dd2
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2023-04-17 00:48:47 -0600

    Fixed issue with language selection

--- path/className_Tests.cs
+++ path/className_Tests.cs
@@ -1,4 +1,4 @@
-// Generation date: 2023-04-16T17:26:05.898097500.
+// Generation date: 2023-04-17T00:35:11.596281400.
 // Input language: null | Output language: C#.
 
 namespace package.name
--- path/className_Tests.java
+++ path/className_Tests.java
@@ -1,4 +1,4 @@
-// Generation date: 2023-04-16T17:26:05.914061800.
+// Generation date: 2023-04-17T00:35:11.602284.
 // Input language: null | Output language: Java.
 
 package package.name;
